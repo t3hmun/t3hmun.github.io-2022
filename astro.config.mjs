94854dfd980ecb58debe043ee69fa80a98389c50
t3hmun <git@t3hmun.com>
2022-11-22 22:45:48 +0000

    Remove MDX not using it

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
@@ -8,5 +7,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://t3hmun.github.io",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [sitemap(), tailwind()],
 });
